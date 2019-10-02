839438a5d6095bb710e0237557440cfee6b01956
David <DMcCulloch86@gmail.com>
2019-10-02 15:55:16 -0400

    added document start function to js and debugging div selector in js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,57 +9,69 @@
 //make a play again button that just reloads the page!!!
 //change innerHTML to show 'select an opponent', but only when you have no current opponent.
 
+//loads js after html and css
+$(document).ready( function() {
 
-//set each character and their stats to objects
-let grievous = {
-    health: 250,
-    attack: 20,
-    counterAttack: 25,
-}
+    //set each character and their stats to objects
+    let grievous = {
+        health: 250,
+        attack: 20,
+        counterAttack: 25,
+    }
 
-let windu = {
-    health: 250,
-    attack: 20,
-    counterAttack: 25,
-    
-}
+    let windu = {
+        health: 250,
+        attack: 20,
+        counterAttack: 25,
+        
+    }
 
-let dooku = {
-    health: 250,
-    attack: 20,
-    counterAttack: 25,
-    
-}
+    let dooku = {
+        health: 250,
+        attack: 20,
+        counterAttack: 25,
+        
+    }
 
-let yoda = {
-    health: 250,
-    attack: 20,
-    counterAttack: 25,
-    
-}
+    let yoda = {
+        health: 250,
+        attack: 20,
+        counterAttack: 25,
+        
+    }
 
-//define functions
+    //define functions
 
-function displayValue() {
-    $('#grievous-health').text(grievous.health);
-    $('#windu-health').text(windu.health);
-    $('#dooku-health').text(dooku.health);
-    $('#yoda-health').text(yoda.health);
-}
+    function displayValue() {
+        $('#grievous-health').text(grievous.health);
+        $('#windu-health').text(windu.health);
+        $('#dooku-health').text(dooku.health);
+        $('#yoda-health').text(yoda.health);
+    }
 
-//Start Game
-displayValue();
+    //Start Game
+    displayValue();
 
-//when a character is selected, it moves to the 'your character div'
-$('.character').on('click', function(){
-    let moveAllDiv = $('.character').detach('#select-character')
-    moveAllDiv.appendTo('#enemies')
-    $('.character').addClass('enemy')
-    $('.character').removeClass('character')
-    let moveDiv = $(this).detach('#enemies');
-    moveDiv.appendTo('#your-character')
-    $(this).removeClass('enemy')
-    $(this).addClass('hero')
-   
-    
-})
+    //when a character is selected, it moves to the 'your character div'
+    $('.character').on('click', function(){
+        let moveDiv = $(this).detach('#enemies');
+        moveDiv.appendTo('#your-character');
+        $(this).removeClass('character');
+        $(this).addClass('hero');
+        let moveAllDiv = $('.character').detach('#select-character');
+        moveAllDiv.appendTo('#enemies');
+        $('.character').addClass('enemy');
+        $('.character').removeClass('character');
+
+    })
+
+    //Once an enemy, make it a defender to fight.
+    //add an if statnement that this only happens if there is no defender, also change so can't be changed out of defender div
+    $('.enemy').on('click', function() {
+        let moveDiv = $(this).detach('#enemies');
+        moveDiv.appendTo('#defender');
+        $(this).addClass('fight')
+
+    })
+
+});
